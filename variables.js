0303dbbc5649665acd1f712a19471301920dfd2c
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-09-02 13:39:23 -0400

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var movie = "Bambi";
+var isEntertaining = true;
+movie = "Snow White";
+var num4 = 4;
+var num6 = 6;
+var product = num4 * num6;
+var favoriteFoods = ["Cheese", "Applesauce", "Fries"];
+favoriteFoods.shift();
+console.log(movie.length);
