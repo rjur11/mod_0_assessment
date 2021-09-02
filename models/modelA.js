1cf54405fed5b297436e72bed1237d29872308d9
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-09-02 13:44:48 -0400

    Add dino object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  type: "T-rex",
+  name: "Rexy",
+  weightinLbs: 5000,
+  ageInYears: 3.5,
+  isScary = true,
+  hasShortArms = true,
+  dinoFeatures = ["Yellow eyes", "Green scales", "Sharp nails"],
+  dinoDiet = ["Meat", "Fish", "Other dinos"],
+}
