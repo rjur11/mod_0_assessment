6cb5250fcb560fd1950a639d07e46d75a964b95f
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-09-02 13:49:19 -0400

    Add dev object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  devName: "Rana",
+  devProgram: "Turing",
+  YearsOfExperience: 0,
+  languagesKnown: 3,
+  isMotivated: true,
+  knowsRuby: false,
+  helpfulSkills: ["Fast typing speed", "Background programming knowledge"],
+  helpfulResources: ["Turing notes", "StackOverflow"], 
+}
